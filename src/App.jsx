b92b629814742d37f1bb03074378775f71c51417
mohammadqasem <158471513+mohammadqasem02@users.noreply.github.com>
2024-09-22 15:41:53 +0300

    anothernew

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-import Banner from "./components/Banner/Banner";
 import Blogs from "./components/Blogs/Blogs";
 import Swipe from "./components/Blogs/Swipe";
 import Footer from "./components/Footer/Footer";
@@ -10,7 +9,6 @@ import Hero from "./components/Hero.jsx/Hero";
 import Navbar2 from "./components/navbar/Navbar2";
 import { useEffect } from "react";
 import Service from "./components/Service/Service";
-import BannerDetails from "./components/BannerDetails/BannerDetails";
 import Banner1 from "./assets/blog1.jpg";
 import Banner2 from "./assets/blog3.jpg";
 
@@ -33,9 +31,7 @@ function App() {
         </div>
         <Hero />
         <Service />
-        <BannerDetails reverse={true} img={Banner1} />
-        <BannerDetails img={Banner2} />
-        <Banner />
+
         <Blogs />
         {/* <Swipe /> */}
         <Footer />
